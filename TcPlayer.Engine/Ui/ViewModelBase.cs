31a05f4e6ce0f988010ff63525f0f75578ad68ac
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-01-17 20:24:38 +0100

    Layout change

---
+++ TcPlayer.Engine/Ui/ViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TcPlayer.Engine.Ui
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void SetProperty<T>(ref T backfield, T value, [CallerMemberName] string propName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(backfield, value))
+            {
+                backfield = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
