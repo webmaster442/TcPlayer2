af73da97ffb4a3e7e30669e86f4e7ae58286aa19
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-02-24 20:01:44 +0100

    Eqalizer control finalized

--- TcPlayer/Controls/EqualizerControl.xaml.cs
+++ TcPlayer/Controls/EqualizerControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace TcPlayer.Controls
 {
@@ -8,6 +9,17 @@ namespace TcPlayer.Controls
     /// </summary>
     public partial class EqualizerControl : UserControl
     {
+        public ICommand EqualizerChangeCommand
+        {
+            get { return (ICommand)GetValue(EqualizerChangeCommandProperty); }
+            set { SetValue(EqualizerChangeCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty EqualizerChangeCommandProperty =
+            DependencyProperty.Register("EqualizerChangeCommand", typeof(ICommand), typeof(EqualizerControl), new PropertyMetadata(null));
+       
+        private bool _lockChange;
+
         public EqualizerControl()
         {
             InitializeComponent();
@@ -15,38 +27,32 @@ namespace TcPlayer.Controls
 
         private void ResetClick(object sender, RoutedEventArgs e)
         {
+            _lockChange = true;
             Slider0.Value = 0;
             Slider1.Value = 0;
             Slider2.Value = 0;
             Slider3.Value = 0;
             Slider4.Value = 0;
+            _lockChange = false;
+            EqualizerChangeCommand?.Execute(GetValues());
         }
 
-        public float[] Values
+        private float[] GetValues()
         {
-            get
+            return new float[]
             {
-                return new float[]
-                {
-                    (float)Slider0.Value,
-                    (float)Slider1.Value,
-                    (float)Slider2.Value,
-                    (float)Slider3.Value,
-                    (float)Slider4.Value,
-                };
-            }
-            set
-            {
-                if (value != null 
-                    && value.Length == 5)
-                {
-                    Slider0.Value = value[0];
-                    Slider1.Value = value[1];
-                    Slider2.Value = value[2];
-                    Slider3.Value = value[3];
-                    Slider4.Value = value[4];
-                }
-            }
+                (float)Slider0.Value,
+                (float)Slider1.Value,
+                (float)Slider2.Value,
+                (float)Slider3.Value,
+                (float)Slider4.Value,
+            };
+        }
+
+        private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (_lockChange) return;
+            EqualizerChangeCommand?.Execute(GetValues());
         }
     }
 }
