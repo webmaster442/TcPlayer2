6c82a4130b9f31d2e8b618b67cbdf4a5d7124970
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-03-10 10:56:18 +0100

    Entrypoint fixes

--- TcPlayer/Entrypoint.cs
+++ TcPlayer/Entrypoint.cs
@@ -1,12 +1,16 @@
-﻿namespace TcPlayer
+﻿using System;
+
+namespace TcPlayer
 {
     public static class Entrypoint
     {
+        [STAThread]
         public static void Main(string[] arguments)
         {
             var application = new App();
             application.SetupEngineDependencies();
             application.SetupDependencies();
+            application.InitializeComponent();
             application.Run();
         }
     }
