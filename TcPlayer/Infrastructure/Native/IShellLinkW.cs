3af13e3a73df7bb86c84b56b1d5a83535e379b88
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-03-22 22:33:30 +0100

    Interface and class for creating shell links

---
+++ TcPlayer/Infrastructure/Native/IShellLinkW.cs
@@ -0,0 +1,88 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Copyright (c) 2021 Ruzsinszki Gábor
+// This is free software under the terms of the MIT License. https://opensource.org/licenses/MIT
+// ------------------------------------------------------------------------------------------------
+
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace TcPlayer.Infrastructure.Native
+{
+    [ComImport]
+    [Guid("000214F9-0000-0000-C000-000000000046")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IShellLinkW
+    {
+        [PreserveSig]
+        int GetPath(
+            [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszFile,
+            int cch,
+            ref IntPtr pfd,
+            uint fFlags);
+
+        [PreserveSig]
+        int GetIDList(out IntPtr ppidl);
+
+        [PreserveSig]
+        int SetIDList(IntPtr pidl);
+
+        [PreserveSig]
+        int GetDescription(
+            [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszName,
+            int cch);
+
+        [PreserveSig]
+        int SetDescription([MarshalAs(UnmanagedType.LPWStr)] string pszName);
+
+        [PreserveSig]
+        int GetWorkingDirectory(
+            [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszDir,
+            int cch);
+
+        [PreserveSig]
+        int SetWorkingDirectory([MarshalAs(UnmanagedType.LPWStr)] string pszDir);
+
+        [PreserveSig]
+        int GetArguments(
+            [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszArgs,
+            int cch);
+
+        [PreserveSig]
+        int SetArguments([MarshalAs(UnmanagedType.LPWStr)] string pszArgs);
+
+        [PreserveSig]
+        int GetHotkey(out ushort pwHotkey);
+
+        [PreserveSig]
+        int SetHotkey(ushort wHotkey);
+
+        [PreserveSig]
+        int GetShowCmd(out int piShowCmd);
+
+        [PreserveSig]
+        int SetShowCmd(int iShowCmd);
+
+        [PreserveSig]
+        int GetIconLocation(
+            [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszIconPath,
+            int cch,
+            out int piIcon);
+
+        [PreserveSig]
+        int SetIconLocation(
+            [MarshalAs(UnmanagedType.LPWStr)] string pszIconPath,
+            int iIcon);
+
+        [PreserveSig]
+        int SetRelativePath(
+            [MarshalAs(UnmanagedType.LPWStr)] string pszPathRel,
+            uint dwReserved);
+
+        [PreserveSig]
+        int Resolve(IntPtr hwnd, uint fFlags);
+
+        [PreserveSig]
+        int SetPath([MarshalAs(UnmanagedType.LPWStr)] string pszFile);
+    }
+}
---
+++ TcPlayer/Infrastructure/Native/ShellLinkManager.cs
@@ -0,0 +1,44 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Copyright (c) 2021 Ruzsinszki Gábor
+// This is free software under the terms of the MIT License. https://opensource.org/licenses/MIT
+// ------------------------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace TcPlayer.Infrastructure.Native
+{
+    internal static class ShellLinkManager
+    {
+        [ComImport]
+        [Guid("00021401-0000-0000-C000-000000000046")]
+        public class ShellLink
+        {
+        }
+
+        private static void CreateLink(string PathToFile,
+                                  string PathToLink,
+                                  string Arguments, 
+                                  string Description)
+        {
+            var shellLink = new ShellLink() as IShellLinkW;
+
+            Marshal.ThrowExceptionForHR(shellLink.SetDescription(Description));
+            Marshal.ThrowExceptionForHR(shellLink.SetPath(PathToFile));
+            Marshal.ThrowExceptionForHR(shellLink.SetArguments(Arguments));
+
+            ((IPersistFile)shellLink).Save(PathToLink, false);
+
+            Marshal.ReleaseComObject(shellLink);
+        }
+
+        public static void CreateLink(string file, string name, Environment.SpecialFolder folder)
+        {
+            string dir = Environment.GetFolderPath(folder);
+            string linkFileName = Path.Combine(dir, name);
+            CreateLink(file, linkFileName, string.Empty, string.Empty);
+        }
+    }
+}
